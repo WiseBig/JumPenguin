bd502461542eb36531184b42d30c7d85899d02d2
Shimhyunbo <skydj0519@naver.com>
2024-10-12 09:40:44 +0900

    .

--- Assets/Scripts/DataReset.cs
+++ Assets/Scripts/DataReset.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DataReset : MonoBehaviour
 {
+    public GameObject gameDataResetText;
     public void ResetBestScore()
     {
+        gameDataResetText.gameObject.SetActive(true);
+        StartCoroutine(ShowText());
         PlayerPrefs.DeleteKey("BestScore");
-        ScoreManager.bestScore = 0; // �޸𸮿����� �ʱ�ȭ
-        PlayerPrefs.Save(); // ���� ������ ��ũ�� ����
+        ScoreManager.bestScore = 0; 
+        PlayerPrefs.Save(); 
+    }
+    IEnumerator ShowText()
+    {
+        yield return new WaitForSeconds(1);
+        gameDataResetText.gameObject.SetActive(false);
     }
 }
